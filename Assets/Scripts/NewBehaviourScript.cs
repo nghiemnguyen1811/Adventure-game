e423492031a78c7eefb1e1a6a74473ba649a7184
nghiemnguyen1811 <nghiemnguyenxviii@gmail.com>
2021-09-18 16:00:05 +0700

    Add project

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float jumpForce = 11f;
+    public float moveForce = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
